ea952224ca597728d220821277097ac999a9daf1
Ahmed Shahid <a.razashahid19@gmail.com>
2025-05-06 17:13:35 +0500

    learnt variables

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Integer variable to store age
+    int age = 20;
+
+    // Character variable to store a grade
+    char grade = 'A';
+
+    // Float variable to store height
+    float height = 5.9;
+
+    // Print the values
+    printf("Age: %d\n", age);          // %d is for int
+    printf("Grade: %c\n", grade);      // %c is for char
+    printf("Height: %.f\n", height);  // %.f is for float
+
+    return 0;
+}
